effe5ab64b782a9f64f352b31265405da54fa2d7
Andrew Haine <andrew@andrewhaine.co.uk>
2023-01-10 12:34:45 +0000

    Stop `index` showing in canonical URL

--- hooks/useCanonicalUrl.ts
+++ hooks/useCanonicalUrl.ts
@@ -2,6 +2,6 @@ import { useRouter } from "next/router";
 
 export default function useCanonicalUrl(): string {
     const router = useRouter();
-    const canonicalUrl = (process.env.NEXT_PUBLIC_URL + (router.asPath === "/" ? "": router.asPath)).split("?")[0];
+    const canonicalUrl = (process.env.NEXT_PUBLIC_URL + (router.asPath === "/" || router.asPath === '/index' ? "": router.asPath)).split("?")[0];
     return canonicalUrl;
 }
