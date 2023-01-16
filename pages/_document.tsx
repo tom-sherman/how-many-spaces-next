809563c6216a315ed14c62b20528a3e375c382e9
Andrew Haine <andrew@andrewhaine.co.uk>
2023-01-16 19:45:36 +0000

    Add HTML lang

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -30,7 +30,7 @@ export default class MyDocument extends Document {
 
   render() {
       return (
-          <Html>
+          <Html lang="en">
               <Head>
                   <link rel="preconnect" href="https://fonts.googleapis.com" />
                   <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
